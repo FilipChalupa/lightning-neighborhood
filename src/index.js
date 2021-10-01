bbc6587850514cf284648cdade39968493d8432a
Filip Chalupa <chalupa.filip@gmail.com>
2021-10-01 18:51:16 +0200

    Test connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+import dotenv from 'dotenv'
+import { authenticatedLndGrpc, getWalletInfo } from 'lightning'
+
+dotenv.config()
+
+const { CERT, READONLY_MACAROON, SOCKET } = process.env
+
+const { lnd } = authenticatedLndGrpc({
+	cert: CERT,
+	macaroon: READONLY_MACAROON,
+	socket: SOCKET,
+})
+
+const walletInfo = await getWalletInfo({ lnd })
+
+console.log(walletInfo)
